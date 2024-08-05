10fe72d18f10cdcce7bc7f75b77b2c6c98440329
rajibmalik <rajibmalik@hotmail.co.uk>
2024-08-05 20:51:36 +0100

    Updated vercel config

--- development-project/frontend/src/App.tsx
+++ development-project/frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Library from "./pages/Library";
 import Dashboard from "./pages/Dashboard";
-import AuthCallback from "./components/AuthCallback"; // Import the new component
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
 
@@ -14,7 +13,6 @@ function App() {
           <Route index element={<Homepage />} />
           <Route path="/library" element={<Library />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          {/* <Route path="/auth/steam/callback" element={<AuthCallback />} /> // */}
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
