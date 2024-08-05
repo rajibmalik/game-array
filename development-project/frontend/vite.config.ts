bbb48e26b5f4cbb890d38da4909388fcbb8d45ac
rajibmalik <rajibmalik@hotmail.co.uk>
2024-08-05 16:01:43 +0100

    vite update

--- development-project/frontend/vite.config.ts
+++ development-project/frontend/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure this is consistent with your vercel.json
+  },
   test: {
     globals: true,
     environment: "jsdom",
