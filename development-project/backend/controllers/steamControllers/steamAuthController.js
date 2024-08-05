8cafdc152473321bf5460826551e584f1a83f54c
rajibmalik <rajibmalik@hotmail.co.uk>
2024-08-05 19:03:15 +0100

    updated authentication flow

--- development-project/backend/controllers/steamControllers/steamAuthController.js
+++ development-project/backend/controllers/steamControllers/steamAuthController.js
@@ -8,16 +8,20 @@ const initiateSteamAuth = passport.authenticate('steam');
 
 // Middleware handling Steam authentication callback
 // Upon successful authentication, redirects to '/account' page
-const steamAuthCallback = (req, res) => {
+exports.steamAuthCallback = (req, res) => {
   passport.authenticate('steam', { failureRedirect: '/' })(req, res, () => {
     console.log('Steam Authentication successful');
 
-    // Redirect to the account route to initialise Steam API queries
-    res.redirect('/account');
+    // Send a success response with user data
+    res.status(200).json({
+      status: 'success',
+      message: 'Authentication successful',
+      user: req.user,
+    });
   });
 };
 
-module.exports = {
-  initiateSteamAuth,
-  steamAuthCallback,
-};
+// module.exports = {
+//   initiateSteamAuth,
+//   steamAuthCallback,
+// };
