7becfdb85a42bf77e2345f00c2d5af6ef282de98
rajibmalik <rajibmalik@hotmail.co.uk>
2024-08-05 16:47:30 +0100

    relative url

--- development-project/backend/controllers/steamControllers/userController.js
+++ development-project/backend/controllers/steamControllers/userController.js
@@ -25,5 +25,5 @@ exports.createUser = async (req, res, next) => {
 
 // Renders account page with associated user data
 exports.redirectToAccount = (req, res) => {
-  res.redirect('http://localhost:5173/library');
+  res.redirect('/library');
 };
