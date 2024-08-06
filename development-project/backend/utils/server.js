a05cc19b84d41c9a92144d3045d1726f6cc2a2ef
rajibmalik <rajibmalik@hotmail.co.uk>
2024-08-06 12:03:30 +0100

    updated server

--- development-project/backend/utils/server.js
+++ development-project/backend/utils/server.js
@@ -47,7 +47,6 @@ function createServer() {
       saveUninitialized: false,
       cookie: {
         maxAge: 1 * 60 * 60 * 24 * 1000,
-        secure: process.env.NODE_ENV === 'production', // True as using HTTPS in production
       },
     }),
   );
